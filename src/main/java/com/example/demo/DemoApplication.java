f6e9e51f35a6d93e824bef2087b225a6021b764b
= <hbsowo58@gmail.com>
2026-04-08 14:56:02 +0900

    first commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	//	1부터 5까지 반복하여 출력
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
