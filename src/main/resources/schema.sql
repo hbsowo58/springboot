801cd022fa485bf6be032128fe4f648df577ded5
Beom Sung Heo <48181483+hbsowo58@users.noreply.github.com>
2026-04-13 09:59:27 +0900

    fix : fix typo

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,10 +1,10 @@
--- CREATE TABLE posts (
---                        id BIGINT AUTO_INCREMENT PRIMARY KEY COMMENT '게시글 고유 ID',
---                        title VARCHAR(255) NOT NULL COMMENT '게시글 제목',
---                        content TEXT NOT NULL COMMENT '게시글 내용',
---                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '생성 시각',
---                        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정 시각'
--- );
+CREATE TABLE posts (
+                       id BIGINT AUTO_INCREMENT PRIMARY KEY COMMENT '게시글 고유 ID',
+                       title VARCHAR(255) NOT NULL COMMENT '게시글 제목',
+                       content TEXT NOT NULL COMMENT '게시글 내용',
+                       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '생성 시각',
+                       updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정 시각'
+);
 
 
 INSERT INTO posts (title, content) VALUES
